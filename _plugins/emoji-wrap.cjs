2268489594ca8fdd730a648320fc717ff64f6f01
Benny Powers <web@bennypowers.com>
2023-04-13 15:00:06 +0300

    fix: wrap emoji

--- _plugins/emoji-wrap.cjs
+++ _plugins/emoji-wrap.cjs
@@ -1,8 +1,10 @@
 // https://kittygiraudel.com/2021/01/02/accessible-emojis-with-11ty/
 // Thanks to Kitty Giraudel
 //
-const emojiRegex = require('emoji-regex')
-const emojiShortName = require('emoji-short-name')
+const getRegex = require('emoji-regex')
+const names = require('emoji-short-name')
+
+const linkedom = require('linkedom');
 
 /** @param {import('@11ty/eleventy/src/UserConfig')} eleventyConfig */
 module.exports = function(eleventyConfig, { exclude }) {
@@ -12,13 +14,19 @@ module.exports = function(eleventyConfig, { exclude }) {
       if (!this.page.outputPath.endsWith?.('.html') || (exclude && this.page.outputPath.match?.(exclude))) {
         return content;
       } else {
-        console.log(this.page.outputPath);
-        return content.replace(emojiRegex(), function wrapEmoji(emoji) {
-          const label = emojiShortName[emoji];
-          return label
-            ? `<span class="emoji" role="img" aria-label="${label}" title="${label}">${emoji}</span>`
-            : `<span class="emoji">${emoji}</span>`
+        const RE = getRegex();
+        const { document, Node } = linkedom.parseHTML(content);
+        document.querySelectorAll(':is(a, h1, h2, h3, h4, h5, h6, .magic-color, p)').forEach(el => {
+          el.childNodes.forEach(node => {
+            if (node.nodeType === Node.TEXT_NODE && node.textContent.match(RE)) {
+              const cont = document.createElement('div');
+              cont.innerHTML = node.nodeValue.replace(RE, emoji =>
+                `<span class="emoji" role="img" aria-label="${names[emoji] ?? 'unknown emoji'}">${emoji}</span>`);
+              node.replaceWith(...cont.childNodes);
+            }
+          });
         });
+        return document.toString();
       }
     });
 }
