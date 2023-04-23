39d80443a157e5bd89c7d5f98cc2bb539fafe5d3
Benny Powers <web@bennypowers.com>
2023-04-23 09:15:36 +0300

    chore: allow bypassing jampack in build

--- _plugins/jampack.cjs
+++ _plugins/jampack.cjs
@@ -4,7 +4,7 @@ const exec = promisify(_exec);
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.on('eleventy.after', async function({ runMode }) {
-    if (runMode === 'build') {
+    if (runMode === 'build' && process.env.NO_JAMPACK !== 'true') {
       await exec('npx @divriots/jampack ./_site');
     }
   })
