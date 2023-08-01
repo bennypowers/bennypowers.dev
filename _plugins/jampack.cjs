9d9079983db4bd0332ee0922d37a5f00234161c5
Benny Powers <web@bennypowers.com>
2023-08-01 07:52:57 +0300

    chore: jampack logging

--- _plugins/jampack.cjs
+++ _plugins/jampack.cjs
@@ -6,10 +6,14 @@ module.exports = function(eleventyConfig, options) {
   eleventyConfig.ignores.add('.jampack/**/*');
   eleventyConfig.on('eleventy.after', async function({ runMode }) {
     if (runMode === 'build' && process.env.NO_JAMPACK !== 'true') {
+      console.log('[jampack] starting...');
       let command = 'npx @divriots/jampack ./_site' 
       if (options?.exclude)
         command += ` ${options.exclude}`
+      const start = performance.now();
       await exec(command);
+      const delta = performance.now() - start;
+      console.log(`[jampack] completed in ${(delta / 1000).toFixed(2)}s`);
     }
   })
 }
