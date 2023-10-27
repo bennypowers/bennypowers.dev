3d851a431d415b72995e9da95afd86acba2efe2d
Benny Powers <web@bennypowers.com>
2023-10-27 12:28:15 +0300

    shul: fullscreen button

--- shul/shul.js
+++ shul/shul.js
@@ -1,5 +1,8 @@
 /** @type {NodeListOf<HTMLElement>} */
 const editable = document.querySelectorAll('[contenteditable][data-storage-key]')
+const fullscreen = document.getElementById('fullscreen')
+const exitFullsc = document.getElementById('exit-fullscreen')
+
 for (const field of editable) {
   const { storageKey } = field.dataset;
   const key = `shul-${storageKey}`;
@@ -10,3 +13,15 @@ for (const field of editable) {
     localStorage.setItem(key, field.textContent);
   });
 }
+
+fullscreen.addEventListener('click', async function() {
+  await document.body.requestFullscreen();
+  exitFullsc.hidden = false;
+  fullscreen.hidden = true;
+});
+
+exitFullsc.addEventListener('click', async function() {
+  await document.exitFullscreen();
+  exitFullsc.hidden = true;
+  fullscreen.hidden = false;
+});
