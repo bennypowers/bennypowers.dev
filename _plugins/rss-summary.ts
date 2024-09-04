f89e10b8c2c4c883ee92395aee2b3885de56518b
Benny Powers <web@bennypowers.com>
2024-09-04 08:03:51 +0300

    fix: link feed

--- _plugins/rss-summary.ts
+++ _plugins/rss-summary.ts
@@ -1,18 +1,13 @@
 import type { UserConfig } from '@11ty/eleventy';
-import type { CollectionApi } from './posts.ts';
-
-import { parseFragment, serialize } from 'parse5'
-import { isElementNode, query, getTextContent } from '@parse5/tools'
-import { readFile } from 'node:fs/promises';
 import markdownit from 'markdown-it';
 
 export function RSSSummaryPlugin(eleventyConfig: UserConfig) {
-  eleventyConfig.addFilter('rssSummary', (content, page) => {
+  eleventyConfig.addFilter('rssSummary', (content: string, page: any) => {
     const { tldr, description } = page.data
     if (tldr ?? description)
       return markdownit().render(tldr ?? description);
     else
-      return content
+      return content;
   });
 };
 
